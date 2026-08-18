d1043ac905d64d8ef2c2597546377a9787dbc28a
SSWorks <106533596+StormSoftworks@users.noreply.github.com>
2026-08-18 22:51:12 +0300

    Create wishlist.lua

---
+++ whitelist/wishlist.lua
@@ -0,0 +1,11 @@
+local List = {
+	["NzA5NjU1OTYzOA=="] = {
+		Title = "LUNAR PRIVATE",
+		ColorCode = Color3.fromRGB(169, 94, 255),
+
+		Commands = true,
+		commandSuperiority = false,
+	},
+}
+
+return List
